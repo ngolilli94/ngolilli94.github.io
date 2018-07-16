20efcaae6fc701eba4afc78d8692375623599ae9
Lillian N <ngo.lilli94@gmail.com>
2018-07-16 16:34:54 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+class Pokemon{
+ constructor(hp, attack, defense, abilities) {
+     this.hp = hp;
+     this.attack = attack;
+     this.defense = defense;
+     this.abilities = abilities;
+ }   
+}
+
+
+}
